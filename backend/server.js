8057b5ad5ca9dbb9aafb5a771f97874a71dfed38
StasyEgo <118133634+StasyEgo@users.noreply.github.com>
2026-06-26 10:11:35 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+
+// чтобы backend мог отдавать index.html
+app.use(express.static(path.join(__dirname, "../")));
+
+
 const db = new sqlite3.Database("./database.db");
+
 
 db.run(`
 CREATE TABLE IF NOT EXISTS checks(
@@ -16,18 +23,44 @@ CREATE TABLE IF NOT EXISTS checks(
 )
 `);
 
-app.get("/checks", (req,res)=>{
-    db.all("SELECT * FROM checks", [], (err, rows)=>{
-        if(err) return res.status(500).send(err);
-        res.json(rows);
-    });
+
+
+app.get("/", (req,res)=>{
+    res.sendFile(
+        path.join(__dirname,"../index.html")
+    );
 });
 
-app.post("/checks",(req,res)=>{
-    const {id, checked} = req.body;
 
-    db.run(`
-    INSERT INTO checks(id, checked)
+
+app.get("/checks",(req,res)=>{
+
+    db.all(
+        "SELECT * FROM checks",
+        [],
+        (err,rows)=>{
+
+            if(err){
+                res.status(500).send(err);
+                return;
+            }
+
+            res.json(rows);
+        }
+    );
+
+});
+
+
+
+app.post("/checks",(req,res)=>{
+
+    const {id,checked}=req.body;
+
+
+    db.run(
+    `
+    INSERT INTO checks(id,checked)
     VALUES(?,?)
     ON CONFLICT(id)
     DO UPDATE SET checked=?
@@ -38,11 +71,22 @@ app.post("/checks",(req,res)=>{
         checked ? 1 : 0
     ],
     err=>{
-        if(err) return res.status(500).send(err);
-        res.json({success:true});
+
+        if(err){
+            res.status(500).send(err);
+            return;
+        }
+
+        res.json({
+            success:true
+        });
+
     });
+
 });
 
-app.listen(3000, ()=>{
-    console.log("Backend started on port 3000");
+
+
+app.listen(3000,()=>{
+    console.log("Server started");
 });
